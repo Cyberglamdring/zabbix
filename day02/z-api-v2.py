fec3daca65f4fdd352ad0433bbab615302260a7e
Hleb Kanonik <50049166+Cyberglamdring@users.noreply.github.com>
2019-06-29 17:09:10 +0300

    Add files via upload

---
+++ day02/z-api-v2.py
@@ -0,0 +1,27 @@
+from pyzabbix import ZabbixAPI
+
+zapi = ZabbixAPI("http://192.168.50.100")
+zapi.login("Admin", "zabbix")
+
+zapi.hostgroup.create({
+    "name": "CloudHosts-2"
+})
+
+zapi.host.create({
+                "host": "Tomcat-2",
+                "interfaces":
+                [{
+                        "type": 1,
+                        "main": 1,
+                        "useip": 1,
+                        "ip": "192.168.50.101",
+                        "dns": "",
+                        "port": 10001
+                }],
+                "groups": [{
+                    "groupid": "12"
+                }],
+                "templates": [{
+                    "templateid": '10001'
+                }],
+        })
